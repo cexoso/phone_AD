11f05dc1ce8e193d1a5fb04d066ae6673c21645f
cexoso <285516193@qq.com>
2015-08-17 16:53:49 +0800

    nd

--- app.js
+++ app.js
@@ -2,24 +2,44 @@
     window.indexHtmlInit=function(){
         $(function(){    
             $('#clickArea').click(function(){
-                $('.mask,.container').show();
+                $.ajax({
+                    type: 'POST',
+                    url: '/projects',
+                    data: { name: 'Zepto.js' },
+                    dataType: 'json',                    
+                    success: function(data){
+                        console.log(data);
+                        window.location.href="tips.html";
+                    },
+                    error: function(xhr, type){
+                        console.log(xhr)
+                    }
+                })
             });
-            $('.mask').click(function(){
-                $('.mask,.container').hide();
-            });   
-            // $('#fresh').click(function(){
-            //     window.location.reload();
-            // })
         })    
     }
-    function showCode(str){
-        $('#code').text(str);
-        $('#codeMask').animate({
-            width:0,
-            backgroundPosition:'-95vw 100%'
-        },1000);
+    function getQueryStr(name, str) {
+        str = str || location.search;
+        var res = str.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+        if (res == null || res.length <= 1) {
+            return "";
+        }
+        return res[1];
     }
     window.tipsHtmlInit=function(){
-        showCode('KCf970Hdd1');
+        var code=getQueryStr('errcode');
+        var defaultSrc="res/tishi.jpg";
+        var srcMap={
+            '0':defaultSrc,
+            '1':'res/errbg.jpg'
+        };
+        
+        var src=srcMap[code]||defaultSrc;
+        $('#bg').attr({src:src}).show();
+        if(code=1){
+            var a=$('<div class="errorTips">')
+            .text('123123')
+            .appendTo($('body'));
+        }
     }
 })(window)
